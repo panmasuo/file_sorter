8ebe2d7438d9666dba2473904045bdb58cc7977f
Paweł Zarembski <pawezarem@gmail.com>
2021-10-31 17:32:09 +0100

    sorter: skeleton

---
+++ photo_video_sorter.py
@@ -0,0 +1,25 @@
+
+class Sorter:
+    def __init__(self) -> None:
+        pass
+
+    def get_source_dir(self):
+        pass
+
+    def get_destination_dir(self):
+        pass
+
+    def source_files(self):
+        pass
+
+    def classify_file_by_extension(self, extension):
+        pass
+
+class Photo:
+    pass
+
+class Video:
+    pass
+
+class Trash:
+    pass
