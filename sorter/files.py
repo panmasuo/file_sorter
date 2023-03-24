010f1779e8f3a22371330209b0bcdab45c0b8d6a
Paweł Zarembski <pawezarem@gmail.com>
2023-03-24 23:34:41 +0100

    naming: create name from datetime, using stat/exif/hachori

--- sorter/files.py
+++ sorter/files.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 from sorter.classifier import get_file_classifier
-from sorter.datename import create_name
+from sorter.naming import create_name
 
 
 class FileType:
@@ -9,27 +9,23 @@ class FileType:
     def __init__(self, file_path: Path):
         self._file = file_path
         self._file_class = get_file_classifier(self._file)
-        self._name = create_name(self._file)
+        self._generated_name = create_name(self._file)
 
     def __str__(self):
         return self._file.name
 
     def __repr__(self):
-        return f"{self._file.name} | {self._file_class} | {self._name}"
+        return f"{self._file.name} {self._file_class} {self._generated_name}"
 
     def copy(self):
         self._file
         raise NotImplementedError
 
-    def rename(self, name: str):
-        # TODO: take name from meta/exif/parser
+    def rename(self):
         try:
-            self._file = self._file.rename(name)
+            self._file = self._file.rename(self._generated_name)
         except FileExistsError:
             # probably same file, do not override
             print(f"File {self} already exists")  # TODO: logger
         except PermissionError:
             print(f"Can't access file {self}")  # TODO: logger
-
-    def extract_name(self):
-        raise NotImplementedError
---
+++ sorter/naming.py
@@ -0,0 +1,76 @@
+from datetime import datetime
+from pathlib import Path
+from plum import exceptions
+
+from exif import Image
+from hachoir.parser import createParser
+from hachoir.metadata import extractMetadata
+from hachoir.core import config
+
+
+def create_name(file_: Path) -> str:
+    """Extracts date information from different sources,
+    compares the dates then creating name using oldest
+    date, timestamp and file suffix.
+
+    Args:
+        file_ (Path): File path.
+
+    Returns:
+        str: Name created from date and time.
+    """
+    # functions that can return datetime
+    date_functions = [_meta_date, _hachoir_date, _exif_date]
+
+    creation_dates = [creation for func in date_functions
+                      if (creation := func(file_))]
+
+    date = min(creation_dates)
+
+    return (date.strftime("%Y-%m-%d_%H-%M-%S") +
+            f"_{int(date.timestamp())}" +  # a timestamp for uniqueness
+            f"{file_.suffix}")
+
+
+def _meta_date(file_: Path) -> datetime | None:
+    """Gets date using file stat metadata."""
+    create_time = file_.stat().st_ctime
+    modify_time = file_.stat().st_mtime
+
+    timestamp = min(create_time, modify_time)
+    return datetime.fromtimestamp(timestamp)
+
+
+def _hachoir_date(file_: Path) -> datetime | None:
+    """Gets date using hachoir parser module."""
+    config.quiet = True  # suppress annoying [warn] log message
+
+    try:
+        if (parser := createParser(file_)) is None:
+            return
+
+        metadata = extractMetadata(parser)
+        for line in metadata.exportPlaintext():
+            if '- Creation date' in (created := line.split(':')):
+                date_str = f"{created[1]}:{created[2]}:{created[3]}"
+                return datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
+
+    except AttributeError:
+        # raised on InputPipe object without "close" attribute
+        pass
+
+
+def _exif_date(file_: Path) -> datetime | None:
+    """Gets date using exif metadata."""
+    try:
+        exif_image = Image(file_)
+        if exif_image.has_exif:
+            return datetime.strptime(exif_image.datetime, '%Y:%m:%d %H:%M:%S')
+
+    except AttributeError:
+        # called on exif objects without datetime attribute
+        pass
+
+    except exceptions.UnpackError:
+        # called on while unpacking
+        pass
