a7a22ad7656f3085bad292f502d7b965d54f4e83
pawezarem <pawezarem@gmail.com>
2025-04-06 23:21:26 +0200

    add runtimewarning photo, add missign test files

--- tests/sorter/conftest.py
+++ tests/sorter/conftest.py
@@ -2,27 +2,34 @@ import pytest
 from datetime import datetime
 from pathlib import Path
 
-# TODO find picture that creates that error:
-# D:\workspace\file_sorter\env\Lib\site-packages\exif\_image.py:171:
-#   RuntimeWarning: ASCII tag contains -1 fewer bytes than specified
-#       retval = getattr(self, attribute)
 
 TEST_IMAGE_FILES = {
-    # image with "[err!] [/exif/content/exif[0]/value[27]/data[2]]"
+    # image with hachoir "[err!] [/exif/content/exif[0]/value[27]/data[2]]"
     # Unable to create value: float division by zero
     "image_001": {
-        "path": Path("tests/data/test-image-001.jpg"),
+        "path": Path("tests/media/test-image-001.jpg"),
         "name": "2021-08-21_1629560464220000000.jpg",
         "datetime": datetime(year=2021, month=8, day=21,
                              hour=17, minute=41, second=4),
         "output": Path("tests/output/video/2021"),
+    },
+
+    # image with exif error: env\Lib\site-packages\exif\_image.py:171:
+    #   RuntimeWarning: ASCII tag contains -1 fewer bytes than specified
+    #       retval = getattr(self, attribute)
+    "image_002": {
+        "path": Path("tests/media/test-image-002.jpg"),
+        "name": "2016-05-21_1463817789000000000.jpg",
+        "datetime": datetime(year=2016, month=5, day=21,
+                             hour=10, minute=3, second=9),
+        "output": Path("tests/output/photo/2016"),
     },
 }
 
 TEST_VIDEO_FILES = {
     # sanity check
     "video_001": {
-        "path": Path("tests/data/test-video-001.mp4"),
+        "path": Path("tests/media/test-video-001.mp4"),
         "name": "2024-08-17_1723917771000000000.mp4",
         "datetime": datetime(year=2024, month=8, day=17,
                              hour=20, minute=2, second=51),
@@ -30,7 +37,7 @@ TEST_VIDEO_FILES = {
     },
     # not copied when app is running
     "video_002": {
-        "path": Path("tests/data/test-video-002.mp4"),
+        "path": Path("tests/media/test-video-002.mp4"),
         "name": "2021-07-23_1627038653000000000.mp4",
         "datetime": datetime(year=2021, month=7, day=23,
                              hour=13, minute=10, second=53),
